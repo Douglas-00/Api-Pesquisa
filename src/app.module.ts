48c93ddc2f2bae32bfe6c2c130502fc2310d769f
Douglas-00 <alvesdouglas547@gmail.com>
2024-11-06 16:22:13 -0300

    chore: removendo arquivos iniciais e sjuatndo app Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './db/prisma.module';
 import { LoggerModule } from './modules/logger/logger.module';
+import { SearchModule } from './modules/search/search.module';
+import { QuestionModule } from './modules/question/question.module';
+import { ResponseModule } from './modules/response/response.module';
 
 @Module({
-  imports: [PrismaModule, LoggerModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    PrismaModule,
+    LoggerModule,
+    SearchModule,
+    QuestionModule,
+    ResponseModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

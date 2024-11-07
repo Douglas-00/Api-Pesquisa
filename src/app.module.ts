7e5ddcd350a7dc7a6f8493f1e2f3e7fe00ad5660
Douglas-00 <alvesdouglas547@gmail.com>
2024-11-07 12:52:18 -0300

    ajustando app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,9 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './db/prisma.module';
 import { LoggerModule } from './modules/logger/logger.module';
 import { SearchModule } from './modules/search/search.module';
-import { QuestionModule } from './modules/question/question.module';
-import { ResponseModule } from './modules/response/response.module';
 
 @Module({
-  imports: [
-    PrismaModule,
-    LoggerModule,
-    SearchModule,
-    QuestionModule,
-    ResponseModule,
-  ],
+  imports: [PrismaModule, LoggerModule, SearchModule],
   controllers: [],
   providers: [],
 })
